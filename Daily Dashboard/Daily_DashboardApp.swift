29686d5b12ba006b4b91db7bb406a105744d62e4
EaRu723 <y.andrearusso@gmail.com>
2024-11-04 09:08:32 -0500

    Initial Commit

---
+++ Daily Dashboard/Daily_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_DashboardApp.swift
+//  Daily Dashboard
+//
+//  Created by Andrea Russo on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Daily_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
